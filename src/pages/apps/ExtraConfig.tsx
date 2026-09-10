a32167dd48df65bb7808070e060dac08a377529d
S.Saha <soumabhasaha1509+github@gmail.com>
2026-09-10 21:54:29 +0530

    Some minor changes done

--- src/pages/apps/ExtraConfig.tsx
+++ src/pages/apps/ExtraConfig.tsx
@@ -1,7 +1,104 @@
-import React from 'react'
-
+import React from 'react';
+import RemoveCircleTwoToneIcon from '@mui/icons-material/RemoveCircleTwoTone';
+import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNewTwoTone';
+import Switch from '@/pages/shared/Switch';
+import { Box, TextField, Button, Typography, List, ListItem, ListItemAvatar, Avatar, ListItemText, Chip, Divider, Autocomplete, Fab, IconButton, Menu, MenuItem, ListItemIcon, Stack } from '@mui/material';
 export default function ExtraConfig() {
   return (
-    <div>ExtraConfig</div>
+    <Box
+      className="flex flex-col min-h-full items-center w-full p-4 gap-6"
+      color="secondary"
+    >
+      <Typography
+        variant='h5'
+        component="h5"
+        sx={{ borderColor: "divider", backgroundColor: "secondary.main", color: "secondary.contrastText", }}
+        className='w-full max-w-160 p-2 rounded-xl text-center'
+        children={"Extra Config"}
+      />
+
+      <List
+        dense={false}
+        className="w-full max-w-160 rounded-2xl py-1.5"
+        sx={{
+          border: 1,
+          borderColor: "divider",
+          bgcolor: "background.paper",
+        }}
+      >
+        {/* <Controller
+          name="isActive"
+          control={control}
+          render={({ field }) => (
+          )}
+        /> */}
+        <ListItem
+          className="h-14 px-3"
+          secondaryAction={
+            <Switch
+            // {...field} checked={Boolean(field.value)}
+            />
+          }>
+          <ListItemAvatar className="min-w-0 mr-3">
+            <Avatar
+              variant="rounded"
+              className="w-12 h-12 rounded-xl"
+            // sx={{ bgcolor: (field.value ? "green" : "red") }}
+            >
+              <PowerSettingsNewIcon fontSize='medium' />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText
+            id="switch-list-label-password"
+            primary={
+              <Typography variant="body1" className="font-medium">
+                Is rule active?
+              </Typography>
+            }
+            secondary={
+              <Typography
+                variant="body2"
+                className="font-medium"
+              >
+                {/* {field.value ? "Yes" : "No"} */}
+              </Typography>
+            }
+          />
+        </ListItem>
+        <Divider component="li" sx={{ borderColor: "divider", width: "100%", borderWidth: 1, my: 0.5 }} />
+        <ListItem
+          className="h-14 px-3"
+          secondaryAction={
+            <Switch checked />
+          }>
+          <ListItemAvatar className="min-w-0 mr-3">
+            <Avatar
+              variant="rounded"
+              className="w-12 h-12 rounded-xl"
+            >
+              <RemoveCircleTwoToneIcon fontSize='medium' />
+              {/* {(field.value) ? <RemoveCircleTwoToneIcon fontSize='medium' /> : <DoneIcon fontSize='medium' />} */}
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText
+            id="switch-list-label-password"
+            primary={
+              <Typography variant="body1" className="font-medium">
+                Preffered action
+              </Typography>
+            }
+            secondary={
+              <Typography
+                variant="body2"
+                className="font-medium"
+              >
+                Block
+                {/* {field.value ? "Block" : "Allow"} */}
+              </Typography>
+            }
+          />
+        </ListItem>
+      </List>
+    </Box>
   )
 }
--- src/pages/apps/Sync.tsx
+++ src/pages/apps/Sync.tsx
@@ -1,66 +1,93 @@
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
 import { useEffect, useState } from "react";
 import JsonView from "@uiw/react-json-view";
 import { getSyncedData } from "@/utils/sync";
+import TabContext from '@mui/lab/TabContext';
 import { darkTheme } from "@uiw/react-json-view/dark";
+import DownloadIcon from '@mui/icons-material/Download';
+import UploadIcon from '@mui/icons-material/FileUpload';
 import { lightTheme } from "@uiw/react-json-view/light";
-import { Paper, useTheme, Box, Typography, Divider } from "@mui/material";
+import { Paper, useTheme, Box, Tab, Button } from "@mui/material";
 
 export default function Sync() {
   const theme = useTheme();
+  const [value, setValue] = useState('1');
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
+
   const isDarkMode = theme.palette.mode === "dark";
   const [data, setData] = useState<Record<string, any>>({});
   useEffect(() => {
-    getSyncedData().then((sd) => {
-      setData(sd);
-      console.log(sd);
-    });
+    getSyncedData().then(setData);
   }, []);
 
   return (
-    <Box className="flex flex-col items-center gap-6 p-4 w-full">
-      <Typography
-        variant='h5'
-        component="h5"
-        sx={{ borderColor: "divider", backgroundColor: "secondary.main", color: "secondary.contrastText", }}
-        className='w-full max-w-160 p-2 rounded-xl text-center'
-        children={"Export Data"}
-      />
-
-      <Paper
-        elevation={0}
-        className="w-full max-w-160 rounded-2xl"
-        variant="outlined"
-        sx={{
-          p: 2,
-          backgroundColor: isDarkMode
-            ? theme.palette.grey[900]
-            : theme.palette.grey[50],
-          borderColor: theme.palette.divider,
-          fontFamily: "monospace",
-          fontSize: "0.875rem",
-          overflowX: "auto",
-          // Inherit MUI background into the viewer
-          "& .w-rjv": {
-            backgroundColor: "transparent !important",
-          },
-        }}
-      >
-        <JsonView
-          value={data}
-          style={isDarkMode ? darkTheme : lightTheme}
-          collapsed={2}
-          displayDataTypes={false}
-          enableClipboard={true}
-        />
-      </Paper>
-      <Divider className="w-full max-w-160" sx={{ borderColor: "divider", borderWidth: 1 }} />
-      <Typography
-        variant='h5'
-        component="h5"
-        sx={{ borderColor: "divider", backgroundColor: "secondary.main", color: "secondary.contrastText", }}
-        className='w-full max-w-160 p-2 rounded-xl text-center'
-        children={"Import Data"}
-      />
+    <Box className="flex flex-col gap-6 p-4 w-full items-center-safe">
+      <TabContext value={value}>
+        <TabList
+          onChange={handleChange}
+          aria-label="lab tabs"
+          className='min-w-160'
+          sx={{ borderBottom: 1, borderColor: 'divider' }}
+        >
+          <Tab label="Export Data" value="1" />
+          <Tab label="Import Data" value="2" />
+        </TabList>
+        <TabPanel value="1" tabIndex={0} className='min-w-160'>
+          <Paper
+            elevation={0}
+            className="w-full rounded-2xl"
+            variant="outlined"
+            sx={{
+              p: 2,
+              backgroundColor: isDarkMode
+                ? theme.palette.grey[900]
+                : theme.palette.grey[50],
+              borderColor: theme.palette.divider,
+              fontFamily: "monospace",
+              fontSize: "0.875rem",
+              overflowX: "auto",
+              "& .w-rjv": {
+                backgroundColor: "transparent !important",  // Inherit MUI background into the viewer
+              },
+            }}
+          >
+            <JsonView
+              value={data}
+              style={isDarkMode ? darkTheme : lightTheme}
+              collapsed={2}
+              displayDataTypes={false}
+              enableClipboard={true}
+            />
+          </Paper>
+          <br />
+          <Button
+            variant='contained'
+            className='w-full'
+            size='large'
+            type='submit'
+            color='secondary'
+            startIcon={<DownloadIcon />}
+          >
+            Download as json
+          </Button>
+        </TabPanel>
+        <TabPanel value="2" tabIndex={0} className='min-w-160'>
+          <Button
+            variant='contained'
+            className='w-full'
+            size='large'
+            type='submit'
+            color='secondary'
+            startIcon={<UploadIcon />}
+          >
+            Upload json
+          </Button>
+        </TabPanel>
+      </TabContext>
     </Box>
   )
 }
