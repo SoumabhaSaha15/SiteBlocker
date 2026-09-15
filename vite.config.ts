385af52f2a354bc5c8c8ab20fb670a084121cb50
S.Saha <soumabhasaha1509+github@gmail.com>
2026-09-15 11:18:01 +0530

    base framework changed

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,18 @@ import svgr from "vite-plugin-svgr";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import webExtension, { readJsonFile } from "vite-plugin-web-extension";
-
-const pkg = readJsonFile("package.json");
-const manifest = readJsonFile("src/manifest.json");
-
+import pkg from './package.json' with { type: "json" };
+import { crx, defineManifest } from "@crxjs/vite-plugin";
+import baseManifest from './src/manifest.json' with { type: "json" };
+// import webExtension, { readJsonFile } from "vite-plugin-web-extension";
+// const pkg = readJsonFile("package.json");
+// const manifest = readJsonFile("src/manifest.json");
+const manifest = defineManifest({
+  name: pkg.name,
+  description: pkg.description,
+  version: pkg.version,
+  ...baseManifest,
+});
 export default defineConfig((_) => {
   return ({
     resolve: {
@@ -18,16 +25,50 @@ export default defineConfig((_) => {
     plugins: [
       tailwindcss(),
       react(),
-      svgr(),
-      webExtension({
-        manifest: () => ({
-          name: pkg.name,
-          description: pkg.description,
-          version: pkg.version,
-          ...manifest,
-        }),
-        additionalInputs: ["src/index.html", "src/redirect.html"],
+      svgr({
+        include: "src/**/*.svg?react",
+        exclude: "node_modules/**",
+        svgrOptions: {
+          icon: true,
+        },
       }),
+      crx({ manifest }),
+      // webExtension({
+      //   manifest: () => ({
+      //     name: pkg.name,
+      //     description: pkg.description,
+      //     version: pkg.version,
+      //     ...manifest,
+      //   }),
+      //   additionalInputs: ["src/index.html", "src/redirect.html"],
+      // }),
     ],
+    build: {
+      // build.rolldownOptions.output.codeSplitting
+      rollupOptions: {
+        input: {
+          // Use relative paths from project root
+          index: "src/index.html",
+          redirect: "src/redirect.html",
+        },
+        output: {
+          manualChunks(id) {
+            if (id.includes("node_modules")) {
+              if (id.includes("@mui") || id.includes("@emotion")) {
+                return "vendor-mui";
+              }
+              if (id.includes("react") || id.includes("react-dom")) {
+                return "vendor-react";
+              }
+              return "vendor";
+            }
+          },
+        },
+      },
+    },
+    server: {
+      port: 5173,
+      strictPort: true,
+    }
   })
 });
